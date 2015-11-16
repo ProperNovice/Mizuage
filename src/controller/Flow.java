7e57d6be759115e570b81f3bb9a6ed9bbb37a45a
Joulies <Joulies@Joul>
2015-11-16 11:31:02 +0200

    q

--- src/controller/Flow.java
+++ src/controller/Flow.java
@@ -1,8 +1,27 @@
 package controller;
 
+import instances.Instances;
+import utils.ArrayList;
+import enums.GameStateEnum;
+
 public class Flow {
 
+	private ArrayList<GameStateEnum> gameStateTurn = new ArrayList<>();
+	private ArrayList<GameStateEnum> gameStateResolving = new ArrayList<>();
+
 	public Flow() {
+
+	}
+
+	public void proceedToNextPhase() {
+
+		if (this.gameStateResolving.isEmpty())
+			this.gameStateResolving.addAll(this.gameStateTurn);
+
+		Controller controller = Instances.getControllerInstance();
+
+		GameStateEnum gameStateEnum = this.gameStateResolving.removeFirst();
+		controller.gameStateController().setGameState(gameStateEnum);
 
 	}
 
